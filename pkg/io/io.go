fc3f3f755046818d126abff9b08ba5e3cf193fe6
sunfeilong <sunfeilong@eglsgame.com>
2020-10-22 12:12:22 +0800

    文件是否存在

---
+++ pkg/io/io.go
@@ -0,0 +1,16 @@
+package io
+
+import (
+    "os"
+)
+
+func FileExists(filePath string) bool {
+    if _, err := os.Stat(filePath); err == nil || os.IsExist(err) {
+        return true
+    }
+    return false
+}
+
+func FileNotExists(filePath string) bool {
+    return !FileExists(filePath)
+}
---
+++ pkg/io/io_test.go
@@ -0,0 +1,31 @@
+package io
+
+import (
+    "github.com/stretchr/testify/assert"
+    "os"
+    "testing"
+)
+
+func TestFileExists(t *testing.T) {
+    filePath := "awukknxs.txt"
+    assert.False(t, FileExists(filePath), "file should not exists")
+    file, err := os.Create(filePath)
+    file.Close()
+    if err == nil {
+        assert.True(t, FileExists(filePath), "file should exists")
+        os.Remove(filePath)
+    }
+    assert.False(t, FileExists(filePath), "file should not exists")
+}
+
+func TestFileNotExists(t *testing.T) {
+    filePath := "awukknxs.txt"
+    assert.True(t, FileNotExists(filePath), "file should not exists")
+    file, err := os.Create(filePath)
+    file.Close()
+    if err == nil {
+        assert.False(t, FileNotExists(filePath), "file should exists")
+        os.Remove(filePath)
+    }
+    assert.True(t, FileNotExists(filePath), "file should not exists")
+}
