f9145917ab85c1165f2c63ab697a14441d9d7088
sunfeilong <sunfeilong1993@gmail.com>
2021-01-31 15:25:14 +0800

    README wendang

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # lilac
 
-丁香花，go工具类。
+lilac,go tools.
+
+latest version: `v1.1.7`
+
+import: `github.com/bittygarden/lilac/io_tool`
+
+## date_tool
+
+* `date_tool.NowInE8() time.Time` current time in EAST 8 time zone.
+* `date_tool.NowDateTime() string` return current time in EAST 8 time zone with yyyy-MM-dd HH:mm:ss format.
+* `date_tool.NowDate() string` return current time in EAST 8 time zone with yyyy-MM-dd format.
+* `date_tool.NowTime() string` return current time in EAST 8 time zone with HH:mm:ss format.
+* `date_tool.DateTime(time time.Time) string` format time to yyyy-MM-dd HH:mm:ss.
+* `date_tool.Date(time time.Time) string` format time to yyyy-MM-dd.
+* `date_tool.Time(time time.Time) string` format time to HH:mm:ss.
+* `date_tool.ParseDateTime(timeStr string) time.Time` parse yyyy-MM-dd HH:mm:ss format time string to time, eg: "2006-01-02 15:04:05".
+* `date_tool.ParseDate(timeStr string) time.Time` parse yyyy-MM-dd format time string to time, eg: "2006-01-02".
+* `date_tool.ParseTime(timeStr string) time.Time` parse HH:mm:ss format time string to time, eg: "15:04:05".
+
+## io_tool
+
+* `io_tool.FileNExists(filePath string) bool` return true if file exists.
+* `io_tool.FileNotExists(filePath string) bool` return true if file not exists.
--- date_tool/date_tool.go
+++ date_tool/date_tool.go
@@ -9,17 +9,17 @@ func NowInE8() time.Time {
     return time.Now().In(E8)
 }
 
-//format current time in EAST 8 time zone to yyyy-MM-dd HH:mm:ss
+//return current time in EAST 8 time zone with yyyy-MM-dd HH:mm:ss format
 func NowDateTime() string {
     return DateTime(time.Now().In(E8))
 }
 
-//format current time in EAST 8 time zone to yyyy-MM-dd
+//return current time in EAST 8 time zone with yyyy-MM-dd format
 func NowDate() string {
     return Date(time.Now().In(E8))
 }
 
-//format current time in EAST 8 time zone to HH:mm:ss
+//return current time in EAST 8 time zone with HH:mm:ss format
 func NowTime() string {
     return Time(time.Now().In(E8))
 }
@@ -39,12 +39,7 @@ func Time(time time.Time) string {
     return time.Format("15:04:05")
 }
 
-//format time to yyyy-MM-dd HH:mm:ss, return "date",formatTime
-func NowDateTimeWithLabel() (string, string) {
-    return "date", DateTime(time.Now().In(E8))
-}
-
-//parse time from yyyy-MM-dd HH:mm:ss format time string, eg: "2006-01-02 15:04:05"
+//parse yyyy-MM-dd HH:mm:ss format time string to time, eg: "2006-01-02 15:04:05"
 func ParseDateTime(timeStr string) time.Time {
     result, err := time.ParseInLocation("2006-01-02 15:04:05", timeStr, E8)
     if nil != err {
@@ -53,7 +48,7 @@ func ParseDateTime(timeStr string) time.Time {
     return result
 }
 
-//parse time from yyyy-MM-dd format time string, eg: "2006-01-02"
+//parse yyyy-MM-dd format format time string to time, eg: "2006-01-02"
 func ParseDate(str string) time.Time {
     result, err := time.ParseInLocation("2006-01-02", str, E8)
     if nil != err {
@@ -62,7 +57,7 @@ func ParseDate(str string) time.Time {
     return result
 }
 
-//parse time from HH:mm:ss format time string, eg: "15:04:05"
+//parse HH:mm:ss format time string to time, eg: "15:04:05"
 func ParseTime(str string) time.Time {
     result, err := time.ParseInLocation("15:04:05", str, E8)
 
