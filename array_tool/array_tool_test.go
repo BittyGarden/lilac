40b6b663f2e7c0da3bafa73def9058971673f38a
sunfeilong <sunfeilong1993@gmail.com>
2021-05-10 20:05:05 +0800

    split array test

--- array_tool/array_tool_test.go
+++ array_tool/array_tool_test.go
@@ -15,19 +15,29 @@ func TestSplitArray(t *testing.T) {
 
     splitArray := SplitArray(1, data)
     assert.Equal(t, 100, len(splitArray))
+    assert.Equal(t, 1, len(splitArray[0]))
+    assert.Equal(t, 1, len(splitArray[99]))
 
     splitArray = SplitArray(2, data)
     assert.Equal(t, 50, len(splitArray))
+    assert.Equal(t, 2, len(splitArray[0]))
+    assert.Equal(t, 2, len(splitArray[49]))
 
     splitArray = SplitArray(3, data)
     assert.Equal(t, 34, len(splitArray))
+    assert.Equal(t, 3, len(splitArray[0]))
+    assert.Equal(t, 1, len(splitArray[33]))
 
     splitArray = SplitArray(99, data)
     assert.Equal(t, 2, len(splitArray))
+    assert.Equal(t, 99, len(splitArray[0]))
+    assert.Equal(t, 1, len(splitArray[1]))
 
     splitArray = SplitArray(100, data)
     assert.Equal(t, 1, len(splitArray))
+    assert.Equal(t, 100, len(splitArray[0]))
 
     splitArray = SplitArray(101, data)
     assert.Equal(t, 1, len(splitArray))
+    assert.Equal(t, 100, len(splitArray[0]))
 }
