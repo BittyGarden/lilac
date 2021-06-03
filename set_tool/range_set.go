472b9260b017ea658f210b701fb27e3b0698da8a
sunfeilong <sunfeilong1993@gmail.com>
2021-06-03 19:42:43 +0800

    set

---
+++ set_tool/range_set.go
@@ -0,0 +1,169 @@
+package types
+
+import (
+    "fmt"
+    "strings"
+)
+
+//RangeSet
+type RangeSet struct {
+    rangeSet []*Range
+}
+
+//Range between [start,end]
+type Range struct {
+    start uint64
+    end   uint64
+}
+
+func NewRangeSet() *RangeSet {
+    return &RangeSet{
+        rangeSet: make([]*Range, 0),
+    }
+}
+
+func NewRange(start, end uint64) *Range {
+    return &Range{start: start, end: end}
+}
+
+func (rs *RangeSet) Add(r *Range) {
+    if r == nil {
+        return
+    }
+
+    if r.start > r.end {
+        return
+    }
+
+    if r.start < 0 || r.end < 0 {
+        return
+    }
+
+    if len(rs.rangeSet) == 0 {
+        rs.rangeSet = append(rs.rangeSet, &Range{start: r.start, end: r.end})
+        return
+    }
+
+    insertIndex := -1
+    for index, rr := range rs.rangeSet {
+        if r.start < rr.start && insertIndex == -1 {
+            insertIndex = index
+        }
+        if haveSameIndex(r, rr) {
+            rr.start = Min(r.start, rr.start)
+            rr.end = Max(r.end, rr.end)
+            nextIndex := index + 1
+            if nextIndex < len(rs.rangeSet) && rr.end+1 >= rs.rangeSet[nextIndex].start {
+                temp := rs.rangeSet[nextIndex]
+                if index+2 < len(rs.rangeSet) {
+                    rs.rangeSet = append(rs.rangeSet[0:nextIndex], rs.rangeSet[index+2:]...)
+                } else {
+                    rs.rangeSet = rs.rangeSet[0:nextIndex]
+                }
+                rs.Add(temp)
+            }
+            return
+        }
+    }
+
+    if insertIndex == -1 {
+        insertIndex = len(rs.rangeSet)
+    }
+
+    insert := &Range{
+        start: r.start,
+        end:   r.end,
+    }
+    rs.rangeSet = append(rs.rangeSet, insert)
+    copy(rs.rangeSet[insertIndex+1:], rs.rangeSet[insertIndex:])
+    rs.rangeSet[insertIndex] = insert
+}
+
+//RemoveBefore remove range before index
+func (rs *RangeSet) RemoveBefore(removeRangeEnd uint64) {
+    removeIndex := -1
+    for index, v := range rs.rangeSet {
+        if v.end >= removeRangeEnd {
+            break
+        }
+        removeIndex = index
+    }
+    if removeIndex == -1 {
+        return
+    }
+    removeIndex = removeIndex + 1
+    if removeIndex >= len(rs.rangeSet) {
+        rs.rangeSet = make([]*Range, 0)
+        return
+    }
+    rs.rangeSet = rs.rangeSet[removeIndex:]
+}
+
+func (rs *RangeSet) String() string {
+    builder := strings.Builder{}
+    for _, r := range rs.rangeSet {
+        builder.WriteString("[")
+        builder.WriteString(fmt.Sprintf("%d", r.start))
+        builder.WriteString(string(","))
+        builder.WriteString(fmt.Sprintf("%d", r.end))
+        builder.WriteString("] ")
+    }
+    return builder.String()
+}
+
+func (rs *RangeSet) MissingCount() int {
+    if len(rs.rangeSet) == 0 {
+        return 0
+    }
+    var start uint64 = 1
+    missingCount := 0
+    for _, r := range rs.rangeSet {
+        for {
+            if start < r.start {
+                start++
+                missingCount++
+            }
+            if start == r.start {
+                start = r.end + 1
+                break
+            }
+        }
+    }
+    return missingCount
+}
+
+func (rs *RangeSet) Contains(index uint64) bool {
+    for _, r := range rs.rangeSet {
+        if index >= r.start && index <= r.end {
+            return true
+        }
+    }
+    return false
+}
+
+func (rs *RangeSet) index(index uint64) int {
+    for i, r := range rs.rangeSet {
+        if index >= r.start && index <= r.end {
+            return i
+        }
+    }
+    return -1
+}
+
+func haveSameIndex(a *Range, b *Range) bool {
+    return (a.start >= b.start-1 && a.start <= b.end+1) || (a.end >= b.start-1 && a.end <= b.end+1)
+}
+
+func Min(a, b uint64) uint64 {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func Max(a, b uint64) uint64 {
+    if a > b {
+        return a
+    }
+    return b
+}
---
+++ set_tool/range_set_test.go
@@ -0,0 +1,184 @@
+package types
+
+import (
+    "fmt"
+    "github.com/stretchr/testify/assert"
+    "math/rand"
+    "testing"
+)
+
+func TestRangeSet_add(t *testing.T) {
+    set := NewRangeSet()
+
+    set.Add(&Range{start: 1, end: 1})
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(1), set.rangeSet[0].end)
+
+    set.Add(&Range{start: 3, end: 3})
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(1), set.rangeSet[0].end)
+
+    assert.Equal(t, uint64(3), set.rangeSet[1].start)
+    assert.Equal(t, uint64(3), set.rangeSet[1].end)
+
+    set.Add(&Range{start: 2, end: 2})
+    assert.Equal(t, 1, len(set.rangeSet))
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(3), set.rangeSet[0].end)
+
+    set.Add(&Range{start: 5, end: 5})
+    assert.Equal(t, 2, len(set.rangeSet))
+    assert.Equal(t, uint64(5), set.rangeSet[1].start)
+    assert.Equal(t, uint64(5), set.rangeSet[1].end)
+
+    set.Add(&Range{start: 10, end: 10})
+    assert.Equal(t, 3, len(set.rangeSet))
+    assert.Equal(t, uint64(10), set.rangeSet[2].start)
+    assert.Equal(t, uint64(10), set.rangeSet[2].end)
+
+    set.Add(&Range{start: 4, end: 10})
+    assert.Equal(t, 1, len(set.rangeSet))
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(10), set.rangeSet[0].end)
+
+    set.Add(&Range{start: 1, end: 1})
+    assert.Equal(t, 1, len(set.rangeSet))
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(10), set.rangeSet[0].end)
+
+    set.Add(&Range{start: 10, end: 12})
+    assert.Equal(t, 1, len(set.rangeSet))
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(12), set.rangeSet[0].end)
+
+    set.Add(&Range{start: 13, end: 14})
+    assert.Equal(t, 1, len(set.rangeSet))
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(14), set.rangeSet[0].end)
+
+    set.Add(&Range{start: 100, end: 120})
+    assert.Equal(t, 2, len(set.rangeSet))
+    assert.Equal(t, uint64(100), set.rangeSet[1].start)
+    assert.Equal(t, uint64(120), set.rangeSet[1].end)
+
+    set.Add(&Range{start: 130, end: 140})
+    assert.Equal(t, 3, len(set.rangeSet))
+    assert.Equal(t, uint64(130), set.rangeSet[2].start)
+    assert.Equal(t, uint64(140), set.rangeSet[2].end)
+
+    set.Add(&Range{start: 122, end: 123})
+    assert.Equal(t, 4, len(set.rangeSet))
+    assert.Equal(t, uint64(122), set.rangeSet[2].start)
+    assert.Equal(t, uint64(123), set.rangeSet[2].end)
+
+    set.Add(&Range{start: 15, end: 99})
+    set.Add(&Range{start: 120, end: 140})
+    assert.Equal(t, 1, len(set.rangeSet))
+    assert.Equal(t, uint64(1), set.rangeSet[0].start)
+    assert.Equal(t, uint64(140), set.rangeSet[0].end)
+}
+
+func TestA(t *testing.T) {
+    set := NewRangeSet()
+    set.Add(NewRange(7208, 7208))
+    set.Add(NewRange(5448, 5448))
+    set.Add(NewRange(7762, 7762))
+    set.Add(NewRange(4610, 4610))
+
+    assert.Equal(t, uint64(4610), set.rangeSet[0].start)
+}
+
+func TestRangeSet(t *testing.T) {
+    set := NewRangeSet()
+    for {
+        value := rand.Intn(10000)
+        set.Add(NewRange(uint64(value), uint64(value)))
+
+        r := NewRange(0, 0)
+        for _, v := range set.rangeSet {
+            b := r.end < v.start
+            if !b {
+                fmt.Println(set.String())
+            }
+            assert.True(t, b)
+            r = v
+        }
+        if set.rangeSet[0].start == uint64(0) && set.rangeSet[0].end == uint64(9999) {
+            fmt.Println(set.String())
+            break
+        }
+    }
+}
+
+func TestHaveSameIndex(t *testing.T) {
+    a := &Range{start: 1, end: 1}
+    b := &Range{start: 1, end: 10}
+    assert.True(t, haveSameIndex(a, b))
+}
+
+func TestRemoveBefore(t *testing.T) {
+    set := NewRangeSet()
+    set.Add(NewRange(1, 1))
+    set.Add(NewRange(3, 3))
+    set.Add(NewRange(5, 5))
+    set.Add(NewRange(7, 7))
+
+    set.RemoveBefore(0)
+    assert.Equal(t, 4, len(set.rangeSet))
+
+    set.RemoveBefore(2)
+    assert.Equal(t, 3, len(set.rangeSet))
+
+    set.RemoveBefore(7)
+    assert.Equal(t, 1, len(set.rangeSet))
+
+    set.RemoveBefore(8)
+    assert.Equal(t, 0, len(set.rangeSet))
+}
+
+func TestRemoveBeforeEmpty(t *testing.T) {
+    set := NewRangeSet()
+
+    set.RemoveBefore(0)
+    assert.Equal(t, 0, len(set.rangeSet))
+}
+
+func TestContains(t *testing.T) {
+    set := NewRangeSet()
+
+    set.Add(NewRange(1, 1))
+
+    set.Add(NewRange(3, 3))
+
+    set.Add(NewRange(9, 100))
+    set.Add(NewRange(100, 200))
+
+    set.Add(NewRange(10000, 99999))
+
+    assert.False(t, set.Contains(0))
+    assert.True(t, set.Contains(1))
+    assert.False(t, set.Contains(2))
+    assert.True(t, set.Contains(3))
+    assert.False(t, set.Contains(4))
+
+    assert.False(t, set.Contains(7))
+    assert.False(t, set.Contains(8))
+    assert.True(t, set.Contains(9))
+    assert.True(t, set.Contains(10))
+    assert.True(t, set.Contains(99))
+    assert.True(t, set.Contains(100))
+    assert.True(t, set.Contains(199))
+    assert.True(t, set.Contains(200))
+    assert.False(t, set.Contains(201))
+    assert.False(t, set.Contains(202))
+
+    assert.False(t, set.Contains(8888))
+    assert.False(t, set.Contains(9999))
+    assert.True(t, set.Contains(10000))
+    assert.True(t, set.Contains(88888))
+    assert.True(t, set.Contains(99999))
+
+    assert.False(t, set.Contains(100000))
+    assert.False(t, set.Contains(100001))
+    assert.False(t, set.Contains(500001))
+}
