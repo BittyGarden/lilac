17b787332bf4ca8f66c5be13c13533b22eb755b6
sunfeilong <w_sunfeilong@xiwang.com>
2023-03-21 16:28:54 +0800

    代码检查

--- cache_tool/fixed_duration_cache.go
+++ cache_tool/fixed_duration_cache.go
@@ -1,148 +1,148 @@
 package utils
 
 import (
-    "sync"
-    "time"
+	"sync"
+	"time"
 )
 
 type cacheNode struct {
-    createTime time.Time
-    value      interface{} //cache value
-    next       *cacheNode
-    prev       *cacheNode
+	createTime time.Time
+	value      interface{} //cache value
+	next       *cacheNode
+	prev       *cacheNode
 }
 
-//FixedDurationCache cache data with fixed duration from [now - duration,now]
+// FixedDurationCache cache data with fixed duration from [now - duration,now]
 type FixedDurationCache struct {
-    duration time.Duration //data cache time
-    mutex    sync.Mutex
-    start    *cacheNode
-    end      *cacheNode
-    keyFunc  func(v interface{}) string // get key from value
+	duration time.Duration //data cache time
+	mutex    sync.Mutex
+	start    *cacheNode
+	end      *cacheNode
+	keyFunc  func(v interface{}) string // get key from value
 }
 
 func NewFixedDurationCache(duration time.Duration, keyFunc func(v interface{}) string) *FixedDurationCache {
-    return &FixedDurationCache{
-        duration: duration,
-        keyFunc:  keyFunc,
-    }
+	return &FixedDurationCache{
+		duration: duration,
+		keyFunc:  keyFunc,
+	}
 }
 
-//Add add data to cache
+// Add add data to cache
 func (fdc *FixedDurationCache) Add(v interface{}) {
-    fdc.mutex.Lock()
-    defer fdc.mutex.Unlock()
+	fdc.mutex.Lock()
+	defer fdc.mutex.Unlock()
 
-    node := newCacheNode(v)
+	node := newCacheNode(v)
 
-    //new node
-    if fdc.end == nil {
-        fdc.start = node
-        fdc.end = node
-        return
-    }
+	//new node
+	if fdc.end == nil {
+		fdc.start = node
+		fdc.end = node
+		return
+	}
 
-    //add to end
-    node.prev = fdc.end
-    fdc.end.next = node
-    fdc.end = node
+	//add to end
+	node.prev = fdc.end
+	fdc.end.next = node
+	fdc.end = node
 
-    //after add data to cache we can assert that last data is not expire
-    node = fdc.start
-    for {
-        if fdc.expireTimeLine().Before(node.createTime) {
-            fdc.start = node
-            prev := node.prev
-            //cut link list from node
-            if prev != nil {
-                node.prev.next = nil
-                node.prev = nil
-            }
-            break
-        }
-        node = node.next
-    }
+	//after add data to cache we can assert that last data is not expire
+	node = fdc.start
+	for {
+		if fdc.expireTimeLine().Before(node.createTime) {
+			fdc.start = node
+			prev := node.prev
+			//cut link list from node
+			if prev != nil {
+				node.prev.next = nil
+				node.prev = nil
+			}
+			break
+		}
+		node = node.next
+	}
 }
 
-//Exists if in cache return ture, else return false
+// Exists if in cache return ture, else return false
 func (fdc *FixedDurationCache) ExistsByKey(key string) bool {
-    for node := fdc.end; node != nil; node = node.prev {
-        if node.createTime.Before(fdc.expireTimeLine()) {
-            //not search from expire cache data
-            return false
-        }
-        if key == fdc.keyFunc(node.value) {
-            return true
-        }
-    }
-    return false
+	for node := fdc.end; node != nil; node = node.prev {
+		if node.createTime.Before(fdc.expireTimeLine()) {
+			//not search from expire cache data
+			return false
+		}
+		if key == fdc.keyFunc(node.value) {
+			return true
+		}
+	}
+	return false
 }
 
-//Exists if v in cache return ture, else return false
+// Exists if v in cache return ture, else return false
 func (fdc *FixedDurationCache) Exists(v interface{}) bool {
-    return fdc.ExistsByKey(fdc.keyFunc(v))
+	return fdc.ExistsByKey(fdc.keyFunc(v))
 }
 
-//GetByKey return cache data mapped by key
+// GetByKey return cache data mapped by key
 func (fdc *FixedDurationCache) GetByKey(key string) interface{} {
-    for node := fdc.end; node != nil; node = node.prev {
-        if node.createTime.Before(fdc.expireTimeLine()) {
-            //not search from expire cache data
-            return nil
-        }
-        if key == fdc.keyFunc(node.value) {
-            return node.value
-        }
-    }
-    return nil
+	for node := fdc.end; node != nil; node = node.prev {
+		if node.createTime.Before(fdc.expireTimeLine()) {
+			//not search from expire cache data
+			return nil
+		}
+		if key == fdc.keyFunc(node.value) {
+			return node.value
+		}
+	}
+	return nil
 }
 
-//Diff return cache data who's key not exists in key list
+// Diff return cache data who's key not exists in key list
 func (fdc *FixedDurationCache) Diff(keyList []string) []interface{} {
-    if keyList == nil || len(keyList) == 0 {
-        return fdc.GetAll()
-    }
+	if len(keyList) == 0 {
+		return fdc.GetAll()
+	}
 
-    keyMap := make(map[string]struct{})
-    for _, key := range keyList {
-        keyMap[key] = struct{}{}
-    }
+	keyMap := make(map[string]struct{})
+	for _, key := range keyList {
+		keyMap[key] = struct{}{}
+	}
 
-    result := make([]interface{}, 0)
-    for node := fdc.end; node != nil; node = node.prev {
-        if node.createTime.Before(fdc.expireTimeLine()) {
-            break
-        }
-        nodeKey := fdc.keyFunc(node.value)
-        if _, ok := keyMap[nodeKey]; !ok {
-            result = append(result, node.value)
-        }
-    }
-    return result
+	result := make([]interface{}, 0)
+	for node := fdc.end; node != nil; node = node.prev {
+		if node.createTime.Before(fdc.expireTimeLine()) {
+			break
+		}
+		nodeKey := fdc.keyFunc(node.value)
+		if _, ok := keyMap[nodeKey]; !ok {
+			result = append(result, node.value)
+		}
+	}
+	return result
 }
 
-//GetAll get all value in cache
+// GetAll get all value in cache
 func (fdc *FixedDurationCache) GetAll() []interface{} {
-    result := make([]interface{}, 0)
-    for node := fdc.end; node != nil; node = node.prev {
-        if node.createTime.Before(fdc.expireTimeLine()) {
-            break
-        }
-        result = append(result, node.value)
-    }
-    return result
+	result := make([]interface{}, 0)
+	for node := fdc.end; node != nil; node = node.prev {
+		if node.createTime.Before(fdc.expireTimeLine()) {
+			break
+		}
+		result = append(result, node.value)
+	}
+	return result
 }
 
-//expireTimeLine return time line, before the line data has expire, after the line data not expire
+// expireTimeLine return time line, before the line data has expire, after the line data not expire
 func (fdc *FixedDurationCache) expireTimeLine() time.Time {
-    return time.Now().Add(fdc.duration * -1)
+	return time.Now().Add(fdc.duration * -1)
 }
 
 func newCacheNode(v interface{}) *cacheNode {
-    return &cacheNode{
-        createTime: time.Now(),
-        value:      v,
-        next:       nil,
-        prev:       nil,
-    }
+	return &cacheNode{
+		createTime: time.Now(),
+		value:      v,
+		next:       nil,
+		prev:       nil,
+	}
 }
--- set_tool/interval_set.go
+++ set_tool/interval_set.go
@@ -1,171 +1,162 @@
 package set_tool
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
-//IntervalSet is an interval set, such as {[1,1],[5,100],[108,203],[300,400]}
+// IntervalSet is an interval set, such as {[1,1],[5,100],[108,203],[300,400]}
 type IntervalSet struct {
-    intervalSet []*Interval
+	intervalSet []*Interval
 }
 
-//Interval between [start,end]
+// Interval between [start,end]
 type Interval struct {
-    start uint64
-    end   uint64
+	start uint64
+	end   uint64
 }
 
-//NewIntervalSet create an interval set
+// NewIntervalSet create an interval set
 func NewIntervalSet() *IntervalSet {
-    return &IntervalSet{
-        intervalSet: make([]*Interval, 0),
-    }
+	return &IntervalSet{
+		intervalSet: make([]*Interval, 0),
+	}
 }
 
-//NewIntervalSet create an interval set
+// NewIntervalSet create an interval set
 func NewInterval(start, end uint64) *Interval {
-    return &Interval{start: start, end: end}
+	return &Interval{start: start, end: end}
 }
 
 func (rs *IntervalSet) Add(r *Interval) {
-    if r == nil {
-        return
-    }
+	if r == nil {
+		return
+	}
 
-    if r.start > r.end {
-        return
-    }
+	if r.start > r.end {
+		return
+	}
 
-    if r.start < 0 || r.end < 0 {
-        return
-    }
+	if r.start == 0 || r.end == 0 {
+		return
+	}
 
-    if len(rs.intervalSet) == 0 {
-        rs.intervalSet = append(rs.intervalSet, &Interval{start: r.start, end: r.end})
-        return
-    }
+	if len(rs.intervalSet) == 0 {
+		rs.intervalSet = append(rs.intervalSet, &Interval{start: r.start, end: r.end})
+		return
+	}
 
-    insertIndex := -1
-    for index, rr := range rs.intervalSet {
-        if r.start < rr.start && insertIndex == -1 {
-            insertIndex = index
-        }
-        if haveSameIndex(r, rr) {
-            rr.start = Min(r.start, rr.start)
-            rr.end = Max(r.end, rr.end)
-            nextIndex := index + 1
-            if nextIndex < len(rs.intervalSet) && rr.end+1 >= rs.intervalSet[nextIndex].start {
-                temp := rs.intervalSet[nextIndex]
-                if index+2 < len(rs.intervalSet) {
-                    rs.intervalSet = append(rs.intervalSet[0:nextIndex], rs.intervalSet[index+2:]...)
-                } else {
-                    rs.intervalSet = rs.intervalSet[0:nextIndex]
-                }
-                rs.Add(temp)
-            }
-            return
-        }
-    }
+	insertIndex := -1
+	for index, rr := range rs.intervalSet {
+		if r.start < rr.start && insertIndex == -1 {
+			insertIndex = index
+		}
+		if haveSameIndex(r, rr) {
+			rr.start = Min(r.start, rr.start)
+			rr.end = Max(r.end, rr.end)
+			nextIndex := index + 1
+			if nextIndex < len(rs.intervalSet) && rr.end+1 >= rs.intervalSet[nextIndex].start {
+				temp := rs.intervalSet[nextIndex]
+				if index+2 < len(rs.intervalSet) {
+					rs.intervalSet = append(rs.intervalSet[0:nextIndex], rs.intervalSet[index+2:]...)
+				} else {
+					rs.intervalSet = rs.intervalSet[0:nextIndex]
+				}
+				rs.Add(temp)
+			}
+			return
+		}
+	}
 
-    if insertIndex == -1 {
-        insertIndex = len(rs.intervalSet)
-    }
+	if insertIndex == -1 {
+		insertIndex = len(rs.intervalSet)
+	}
 
-    insert := &Interval{
-        start: r.start,
-        end:   r.end,
-    }
-    rs.intervalSet = append(rs.intervalSet, insert)
-    copy(rs.intervalSet[insertIndex+1:], rs.intervalSet[insertIndex:])
-    rs.intervalSet[insertIndex] = insert
+	insert := &Interval{
+		start: r.start,
+		end:   r.end,
+	}
+	rs.intervalSet = append(rs.intervalSet, insert)
+	copy(rs.intervalSet[insertIndex+1:], rs.intervalSet[insertIndex:])
+	rs.intervalSet[insertIndex] = insert
 }
 
-//RemoveBefore remove interval before index
+// RemoveBefore remove interval before index
 func (rs *IntervalSet) RemoveBefore(removeIntervalEnd uint64) {
-    removeIndex := -1
-    for index, v := range rs.intervalSet {
-        if v.end >= removeIntervalEnd {
-            break
-        }
-        removeIndex = index
-    }
-    if removeIndex == -1 {
-        return
-    }
-    removeIndex = removeIndex + 1
-    if removeIndex >= len(rs.intervalSet) {
-        rs.intervalSet = make([]*Interval, 0)
-        return
-    }
-    rs.intervalSet = rs.intervalSet[removeIndex:]
+	removeIndex := -1
+	for index, v := range rs.intervalSet {
+		if v.end >= removeIntervalEnd {
+			break
+		}
+		removeIndex = index
+	}
+	if removeIndex == -1 {
+		return
+	}
+	removeIndex = removeIndex + 1
+	if removeIndex >= len(rs.intervalSet) {
+		rs.intervalSet = make([]*Interval, 0)
+		return
+	}
+	rs.intervalSet = rs.intervalSet[removeIndex:]
 }
 
 func (rs *IntervalSet) String() string {
-    builder := strings.Builder{}
-    for _, r := range rs.intervalSet {
-        builder.WriteString("[")
-        builder.WriteString(fmt.Sprintf("%d", r.start))
-        builder.WriteString(string(","))
-        builder.WriteString(fmt.Sprintf("%d", r.end))
-        builder.WriteString("] ")
-    }
-    return builder.String()
+	builder := strings.Builder{}
+	for _, r := range rs.intervalSet {
+		builder.WriteString("[")
+		builder.WriteString(fmt.Sprintf("%d", r.start))
+		builder.WriteString(string(","))
+		builder.WriteString(fmt.Sprintf("%d", r.end))
+		builder.WriteString("] ")
+	}
+	return builder.String()
 }
 
 func (rs *IntervalSet) MissingCount() int {
-    if len(rs.intervalSet) == 0 {
-        return 0
-    }
-    var start uint64 = 1
-    missingCount := 0
-    for _, r := range rs.intervalSet {
-        for {
-            if start < r.start {
-                start++
-                missingCount++
-            }
-            if start == r.start {
-                start = r.end + 1
-                break
-            }
-        }
-    }
-    return missingCount
+	if len(rs.intervalSet) == 0 {
+		return 0
+	}
+	var start uint64 = 1
+	missingCount := 0
+	for _, r := range rs.intervalSet {
+		for {
+			if start < r.start {
+				start++
+				missingCount++
+			}
+			if start == r.start {
+				start = r.end + 1
+				break
+			}
+		}
+	}
+	return missingCount
 }
 
 func (rs *IntervalSet) Contains(index uint64) bool {
-    for _, r := range rs.intervalSet {
-        if index >= r.start && index <= r.end {
-            return true
-        }
-    }
-    return false
-}
-
-func (rs *IntervalSet) index(index uint64) int {
-    for i, r := range rs.intervalSet {
-        if index >= r.start && index <= r.end {
-            return i
-        }
-    }
-    return -1
+	for _, r := range rs.intervalSet {
+		if index >= r.start && index <= r.end {
+			return true
+		}
+	}
+	return false
 }
 
 func haveSameIndex(a *Interval, b *Interval) bool {
-    return (a.start+1 >= b.start && a.start <= b.end+1) || (a.end+1 >= b.start && a.end <= b.end+1)
+	return (a.start+1 >= b.start && a.start <= b.end+1) || (a.end+1 >= b.start && a.end <= b.end+1)
 }
 
 func Min(a, b uint64) uint64 {
-    if a < b {
-        return a
-    }
-    return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func Max(a, b uint64) uint64 {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
